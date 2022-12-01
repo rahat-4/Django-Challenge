9afff9290e6ddff2a8dc664afc731c7cdb1dc374
rahat-4 <mainul.rahat4@gmail.com>
2022-12-01 18:16:43 +0600

    Initial project setup complete

---
+++ Track_Corporate_Asset/Track_Corporate_Asset/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Track_Corporate_Asset project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Track_Corporate_Asset.settings')
+
+application = get_wsgi_application()
